cfd2f22127c069453dcb21ebfb7beb0f2c6f66da
fatimamas038 <76620150+fatimamas038@users.noreply.github.com>
2021-08-14 13:29:41 +0530

    removed comment routes added defect routes

--- server/routes/commentRoute.js
+++ server/routes/commentRoute.js
@@ -7,43 +7,27 @@ import {authmiddleware} from "../middleware/reruirelogin.js"
 
 const router=express.Router()
 
-router.put('/comment',authmiddleware,(req,res)=>{
-    const comment={
-        text:req.body.text,
-        postedBy:req.user._id,
-        name:req.user.name
-    }
-    console.log(req.user.name);
-    console.log(comment.postedBy);
-    console.log(req.body.name);
-    //findOneAndUpdate(query, { name: 'jason bourne' }, options, callback)
-    Comment.findOneAndUpdate({name:req.body.name},{
-        $push:{comments:comment}
-    },{
-        new:true
-    }).populate("postedBy","_id name")
-    .populate("comments.postedBy","_id name")
-    .exec((err,result)=>{
-        if(err){
-            return res.status(422).json({error:err})
-        }else{
-            res.json(result)
-        }
-    })
-})
-
-router.get('/allcomments',authmiddleware,(req,res)=>{
+router.get('/alldefects',authmiddleware,(req,res)=>{
     
-    Comment.find({})
-    .populate("postedBy","_id name")
-    .populate("comments.postedBy","_id name")
-    
+    Defect.find().sort({_id:-1}).limit(1)
     .then((docs)=>{
-        res.json({docs})
+        res.json(docs)
     }).catch(err=>{
         console.log(err)
     })
     
+})
+router.put('/alldefects',authmiddleware,(req,res)=>{
+    const entries=req.body.entries
+    console.log(entries);
+    const defect=new Defect({
+       entries:req.body.entries 
+    })
+    
+    defect.save()
+    .then(savedDoc => {
+        res.json(savedDoc)
+      });
 })
 
 export default router
